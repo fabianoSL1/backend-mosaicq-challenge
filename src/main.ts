6e9becd4ed57b9903784dce4fc6b1ad92118c94f
fabiano lima <fabianosl.dev@gmail.com>
2024-05-28 01:02:59 -0300

    feature: cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 import { routes } from "./routes";
 import { exceptionsMiddleware } from "./middlewares/exceptionsMiddleware";
+
 
 const PORT = process.env.APP_PORT;
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use(routes);
